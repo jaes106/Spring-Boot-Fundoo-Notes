436dbd16d42c0f2429b1cae8fe0306730012eeda
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-20 12:09:22 +0530

    [Jason] chore : initial spring boot project setup with base structure

---
+++ src/test/java/com/fundoonotes/FundoonotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundoonotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundoonotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
