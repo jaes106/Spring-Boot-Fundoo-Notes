e5d7e51714a7d2b5a356a07508ae0aded071e0f8
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-24 11:28:34 +0530

    [Jason] refactor : uc14 correct application entry point naming

--- src/main/java/com/fundoonotes/FundoonotesApplication.java
+++ src/main/java/com/fundoonotes/FundoonotesApplication.java
@@ -2,12 +2,12 @@ package com.fundoonotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
-public class FundoonotesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FundoonotesApplication.class, args);
-	}
-
+public class FundooNotesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FundooNotesApplication.class, args);
+    }
 }
