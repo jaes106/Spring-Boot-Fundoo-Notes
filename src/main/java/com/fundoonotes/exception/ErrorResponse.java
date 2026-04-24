53efc58e7b194f1accc9aa977ce248373a4d6e6b
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-24 11:20:47 +0530

    [Jason] feat : uc6 implement global exception handling and custom exceptions

---
+++ src/main/java/com/fundoonotes/exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.fundoonotes.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private int statusCode;
+    private LocalDateTime timestamp;
+}
